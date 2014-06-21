e348f37b8beb597a2ca40922e6f073511713cb39
Matheus Lisboa de Barros <matheuslbarros@gmail.com>
2014-06-21 08:18:51 -0300

    Correcao da tabulacao do arquivo

--- src/br/edu/uniritter/crivo/Crivo.java
+++ src/br/edu/uniritter/crivo/Crivo.java
@@ -9,24 +9,24 @@ import java.util.List;
  */
 public class Crivo {
 
-	public List<Integer> retornaNumerosPrimosDoIntervalo(Integer numeroInicial, Integer numeroFinal) {
-		List<Integer> primos = new LinkedList();
-		
-		for(int numero = (numeroInicial + 1); numero <= numeroFinal; numero++) {
-			primos.add(numero);
-		}
-		
-		Double numeroMaximo = Math.sqrt(numeroFinal);
-		
-		for(int divisor = 0; divisor < primos.size() && primos.get(divisor) <= numeroMaximo; divisor++) {
-			for(int multiplo = (divisor + 1); multiplo < primos.size(); multiplo++) {
-				if (primos.get(multiplo) % primos.get(divisor) == 0) {
-					primos.remove(multiplo);
-				}
-			}
-		}
-		
-		return primos;
-	}
+    public List<Integer> retornaNumerosPrimosDoIntervalo(Integer numeroInicial, Integer numeroFinal) {
+        List<Integer> primos = new LinkedList();
+
+        for (int numero = (numeroInicial + 1); numero <= numeroFinal; numero++) {
+            primos.add(numero);
+        }
+
+        Double numeroMaximo = Math.sqrt(numeroFinal);
+
+        for (int divisor = 0; divisor < primos.size() && primos.get(divisor) <= numeroMaximo; divisor++) {
+            for (int multiplo = (divisor + 1); multiplo < primos.size(); multiplo++) {
+                if (primos.get(multiplo) % primos.get(divisor) == 0) {
+                    primos.remove(multiplo);
+                }
+            }
+        }
+
+        return primos;
+    }
 
 }
--- test/br/edu/uniritter/crivo/CrivoTest.java
+++ test/br/edu/uniritter/crivo/CrivoTest.java
@@ -11,22 +11,22 @@ import org.junit.Test;
  */
 public class CrivoTest {
 
-	@Test
-	public void verificaNumerosPrimosDoUmAoTrinta() {
-		List<Integer> primosExpected = new LinkedList();
-		primosExpected.add(2);
-		primosExpected.add(3);
-		primosExpected.add(5);
-		primosExpected.add(7);
-		primosExpected.add(11);
-		primosExpected.add(13);
-		primosExpected.add(17);
-		primosExpected.add(19);
-		primosExpected.add(23);
-		primosExpected.add(29);
-		
-		Crivo crivo = new Crivo();
-		assertEquals(primosExpected, crivo.retornaNumerosPrimosDoIntervalo(1, 30));
-	}
+    @Test
+    public void verificaNumerosPrimosDoUmAoTrinta() {
+        List<Integer> primosExpected = new LinkedList();
+        primosExpected.add(2);
+        primosExpected.add(3);
+        primosExpected.add(5);
+        primosExpected.add(7);
+        primosExpected.add(11);
+        primosExpected.add(13);
+        primosExpected.add(17);
+        primosExpected.add(19);
+        primosExpected.add(23);
+        primosExpected.add(29);
+
+        Crivo crivo = new Crivo();
+        assertEquals(primosExpected, crivo.retornaNumerosPrimosDoIntervalo(1, 30));
+    }
 
 }
